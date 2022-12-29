b2bfb5c5158ab633c37a40af4cc4a995c89bf82b
Artem Gluschenko <aglusenko61@gmail.com>
2022-12-29 09:53:15 +0300

    final commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,35 +7,55 @@ import 'swiper/modules/pagination/pagination.scss'
 import 'swiper/swiper.scss'
 import Swiper, { Pagination } from 'swiper';
 
-const readMore = document.querySelectorAll('.read-more');
-const repairs = document.querySelector('.swiper-repairs');
-const brands = document.querySelector('.swiper-brands');
-const text = document.querySelector('.article__text');
+// ARTICLE READ-MORE
+const articleContainer = document.querySelector('.article__text')
+const readMoreArticle = document.querySelector('.read-more')
+const readMoreText = document.querySelector('.read-more--text');
 
-function showContent (readMoreState, readMoreBlock) {
-    readMore.forEach(item => {
-        item.addEventListener('click', (e) => {
-            let target = e.target;
-            if(target.classList.contains(readMoreState)) {
-                readMoreBlock.classList.toggle('content--state--height');
-                target.classList.toggle("read-more--state-transform");
-            }
-            if(target.classList.contains("read-more--state-transform")) {
-                target.textContent = "Скрыть";
-            } else {
-                if(target.classList.contains('read-more--text')) {
-                    target.textContent = "Читать далее";
-                } else {
-                    target.textContent = "Показать всё";
-                }
-            }
-        });
-    });
-}
+readMoreArticle.addEventListener('click', function() {
+  let text
 
-showContent('read-more--repairs', repairs);
-showContent('read-more--brands', brands);
-showContent('read-more--text', text);
+  articleContainer.classList.toggle('content--state--height')
+  text = articleContainer.classList.contains('content--state--height')
+    ? 'Скрыть'
+    : 'Читать далее';
+  readMoreText.textContent = text
+  readMoreArticle.classList.toggle('read-more--state-transform')
+})
+
+// BRANDS READ-MORE
+const brandsContainer = document.querySelector('.swiper-brands')
+const readMoreBrands = document.querySelector('#read-more-brands')
+const readMoreTextBrands = document.querySelector('.read-more--brands')
+
+readMoreBrands.addEventListener('click', function() {
+  let text
+
+  brandsContainer.classList.toggle('content--state--height')
+  text = brandsContainer.classList.contains('content--state--height')
+    ? 'Скрыть'
+    : 'Показать всё';
+  readMoreTextBrands.textContent = text
+  readMoreBrands.classList.toggle('read-more--state-transform')
+})
+
+// REPAIRS READ-MORE
+const repairsContainer = document.querySelector('.swiper-repairs')
+const readMoreRepairs = document.querySelector('#read-more-repairs')
+const readMoreTextRepairs = document.querySelector('.read-more--repairs')
+
+readMoreRepairs.addEventListener('click', function() {
+  let text
+
+  repairsContainer.classList.toggle('content--state--height')
+  text = repairsContainer.classList.contains('content--state--height')
+    ? 'Скрыть'
+    : 'Показать всё'
+  readMoreTextRepairs.textContent = text
+  readMoreRepairs.classList.toggle('read-more--state-transform')
+})
+
+
 
 if(window.matchMedia("(max-width: 767px)").matches) {
     const swiper = new Swiper(".swiper", {
